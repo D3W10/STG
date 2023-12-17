43c71415d505cb037225dc21c94d8082b0652d7b
Daniel Nunes <danyribeironunes15@gmail.com>
2023-12-17 23:25:50 +0000

    Conexão arranjada (yay) e alterações na classe

--- src/main/java/com/g01/Connection.java
+++ src/main/java/com/g01/Connection.java
@@ -8,19 +8,26 @@ import java.util.Arrays;
 import java.util.Properties;
 
 public class Connection {
-    protected java.sql.Connection connect(ConnectionInfo info) throws SQLException {
+    protected java.sql.Connection con;
+
+    protected boolean connect(ConnectionInfo info) {
         Properties properties = new Properties();
         properties.put("user", info.getUser());
         properties.put("password", info.getPassword());
 
-        return DriverManager.getConnection(info.getUrl(), properties);
+        try {
+            con = DriverManager.getConnection(info.getUrl(), properties);
+            return true;
+        }
+        catch (SQLException e) {
+            return false;
+        }
     }
 
-    public ResultSet getEntries(java.sql.Connection connection, String entity, String... columns) throws SQLException {
-        try (Statement statement = connection.createStatement()) {
-            String colString = Arrays.toString(columns).substring(1, Arrays.toString(columns).lastIndexOf(']') - 1);
+    public ResultSet getEntries(String entity, String... columns) throws SQLException {
+        Statement statement = con.createStatement();
+        String colString = columns.length != 0 ? Arrays.toString(columns).substring(1, Arrays.toString(columns).lastIndexOf(']') - 1) : "*";
 
-            return statement.executeQuery("SELECT " + colString + " FROM " + entity);
-        }
+        return statement.executeQuery("SELECT " + colString + " FROM " + entity);
     }
 }
