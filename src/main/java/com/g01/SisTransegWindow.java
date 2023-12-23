8d1341547472e1844fd8ee19884e11a33ce9e2d3
Daniel Nunes <danyribeironunes15@gmail.com>
2023-12-23 18:16:40 +0000

    Introdução da funcionalidade de gestão de LCT

--- src/main/java/com/g01/SisTransegWindow.java
+++ src/main/java/com/g01/SisTransegWindow.java
@@ -1,34 +1,86 @@
 package com.g01;
 
+import com.g01.connection.ConnectionInfo;
+import com.g01.connection.models.LocalCtrlTransito;
+
 import javax.swing.*;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static javax.swing.JOptionPane.showMessageDialog;
+
 public class SisTransegWindow {
+    private final ConnectionInfo info;
+
     private JTabbedPane tabbedPanel;
     private JPanel lctPanel;
-    private JPanel gerenciarPanel;
-    private JPanel notificationsPanel;
-    private JPanel contraordenacaoPanel;
+    private JPanel gerenPanel;
+    private JPanel notiPanel;
+    private JPanel contraPanel;
     private JPanel mainPanel;
-    private JButton inserirButton;
-    private JButton editarButton;
-    private JList list1;
-    private JButton eliminarButton;
-    private JButton eliminarButton1;
-    private JButton inserirButton1;
-    private JButton editarButton1;
-    private JList list2;
+    private JButton lctInserirButton;
+    private JButton lctEditarButton;
+    private JButton lctEliminarButton;
+    private JButton gerenEliminarButton;
+    private JButton gerenInserirButton;
+    private JButton gerenEditarButton;
+    private JList<String> gerenList;
     private JPanel homePanel;
-    private JButton eliminarButton2;
-    private JButton inserirButton2;
-    private JButton editarButton2;
-    private JList list3;
-    private JButton eliminarButton3;
-    private JButton inserirButton3;
-    private JButton editarButton3;
-    private JList list4;
+    private JButton notiEliminarButton;
+    private JButton notiInserirButton;
+    private JButton notiEditarButton;
+    private JList<String> notiList;
+    private JButton contraEliminarButton;
+    private JButton contraInserirButton;
+    private JButton contraEditarButton;
+    private JList<String> contraList;
+    private JTable lctTable;
 
     public SisTransegWindow() {
+        info = new ConnectionInfo("sis_transeg", "postgres", "is");
 
+        tabbedPanel.addChangeListener(e -> {
+            String tabTitle = tabbedPanel.getTitleAt(tabbedPanel.getSelectedIndex());
+
+            if (tabTitle.equals(tabbedPanel.getTitleAt(1)))
+                lctTab();
+        });
+
+        lctTable.getSelectionModel().addListSelectionListener(e -> {
+            lctEditarButton.setEnabled(!lctTable.getSelectionModel().isSelectionEmpty());
+            lctEliminarButton.setEnabled(!lctTable.getSelectionModel().isSelectionEmpty());
+        });
+
+        lctEliminarButton.addActionListener(e -> {
+            for (int index : lctTable.getSelectedRows()) {
+                try (LocalCtrlTransito lct = new LocalCtrlTransito()) {
+                    lct.connect(info);
+
+                    lct.delete(lctTable.getValueAt(index, 0).toString());
+                    lctTab();
+                }
+                catch (SQLException ex) {
+                    errorMsg(ex.getMessage());
+                }
+            }
+        });
+    }
+
+    private void lctTab() {
+        try (LocalCtrlTransito lct = new LocalCtrlTransito()) {
+            lct.connect(info);
+
+            ResultSet lctEntries = lct.getEntries(null);
+            lctTable.setModel(lct.getTableModel(lctEntries));
+        }
+        catch (SQLException e) {
+            errorMsg(e.getMessage());
+        }
+    }
+
+    private void errorMsg(String errorMsg) {
+        showMessageDialog(null, "Não foi possível conectar à base de dados:" + "\n\n" + errorMsg, "Falha na conexão", JOptionPane.ERROR_MESSAGE);
     }
 
     public static void main(String[] args) {
@@ -36,7 +88,6 @@ public class SisTransegWindow {
         frame.setContentPane(new SisTransegWindow().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(640, 480);
-        //frame.pack();
         frame.setVisible(true);
     }
 }
--- src/main/java/com/g01/connection/Connection.java
+++ src/main/java/com/g01/connection/Connection.java
@@ -1,13 +1,12 @@
 package com.g01.connection;
 
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.swing.table.DefaultTableModel;
+import java.sql.*;
 import java.util.Arrays;
 import java.util.Properties;
+import java.util.Vector;
 
-public abstract class Connection {
+public abstract class Connection implements AutoCloseable {
     protected java.sql.Connection con;
 
     protected String entity;
@@ -24,43 +23,58 @@ public abstract class Connection {
     }
 
     /**
-     * Inicia uma nova conexão à base de dados
+     * Inicia uma nova conexão à base de dados.
      *
-     * @param info As informações relativas à base de dados
+     * @param info as informações relativas à base de dados
      *
-     * @return Devolve {@literal true} se a conexão foi efetuada com sucesso, {@literal false} caso contrário.
+     * @throws SQLException se ocorreu um erro na conexão à base de dados
      * */
-    public boolean connect(ConnectionInfo info) {
+    public void connect(ConnectionInfo info) throws SQLException {
         Properties properties = new Properties();
         properties.put("user", info.getUser());
         properties.put("password", info.getPassword());
 
-        try {
-            con = DriverManager.getConnection(info.getUrl(), properties);
-            return true;
-        }
-        catch (SQLException e) {
-            return false;
-        }
+        con = DriverManager.getConnection(info.getUrl(), properties);
     }
 
     /**
-     * Fecha a conexão à base de dadps
+     * Fecha a conexão à base de dadps.
      *
      * @throws SQLException se ocorreu um erro na conexão à base de dados
      * */
+    @Override
     public void close() throws SQLException {
         con.close();
     }
 
+    public DefaultTableModel getTableModel(ResultSet resultSet) throws SQLException {
+        ResultSetMetaData metaData = resultSet.getMetaData();
+        Vector<String> columns = new Vector<>();
+        Vector<Vector<Object>> data = new Vector<>();
+
+        for (int column = 1; column <= metaData.getColumnCount(); column++)
+            columns.add(metaData.getColumnName(column).substring(0, 1).toUpperCase() + metaData.getColumnName(column).substring(1));
+
+        while (resultSet.next()) {
+            Vector<Object> vector = new Vector<>();
+
+            for (int i = 1; i <= metaData.getColumnCount(); i++)
+                vector.add(resultSet.getObject(i));
+
+            data.add(vector);
+        }
+
+        return new DefaultTableModel(data, columns);
+    }
+
     public ResultSet getEntries(String... columns) throws SQLException {
         Statement statement = con.createStatement();
-        String colString = columns.length != 0 ? Arrays.toString(columns).substring(1, Arrays.toString(columns).lastIndexOf(']') - 1) : "*";
+        String colString = columns != null ? Arrays.toString(columns).substring(1, Arrays.toString(columns).lastIndexOf(']') - 1) : "*";
 
         return statement.executeQuery("SELECT " + colString + " FROM " + entity);
     }
 
-    public ResultSet create(String... values) throws SQLException {
+    public ResultSet insert(String... values) throws SQLException {
         Statement statement = con.createStatement();
 
         return statement.executeQuery("INSERT INTO " + entity + fields + " VALUES (" + values + ")");
